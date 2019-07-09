4ddd41b5d4106e587e68824e849d7e187ca1b3ff
Devesh Bhor <deveshbhor@outlook.com>
2019-07-09 18:01:43 -0500

    Finished Background Data Loading Routine

--- API APP/SourcesViewController.swift
+++ API APP/SourcesViewController.swift
@@ -15,45 +15,71 @@ class SourcesViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Colleges In Minnesota"
+        self.title = "Arrests for Theft For NFL Teams"
         let query = "https://nflarrest.com/api/v1/crime/topTeams/Theft"
        
         
-        if let url = URL(string: query) {
-            if let data = try? Data(contentsOf: url) {
-                let json = try! JSON(data: data)
-                parse(json: json)
-                return
+        DispatchQueue.global(qos: .userInitiated).async {
+            [unowned self] in
+            
+            if let url = URL(string: query) {
+                if let data = try? Data(contentsOf: url) {
+                    let json = try! JSON(data: data)
+                    self.parse(json: json)
+                    return
+                }
             }
+            self.loadError()
+            
         }
-        loadError()
+        
+
+        
+        
     }
 
 
     
     
     func parse(json: JSON) {
-        for result in json["sources"].arrayValue {
+        for result in json.arrayValue {
             let Team = result["Team"].stringValue
             let Team_name = result["Team_name"].stringValue
             let Team_city = result["Team_city"].stringValue
             let arrest_count = result["arrest_count"].stringValue
             let source = ["Team": Team, "Team_name": Team_name, "Team_city": Team_city, "arrest_count": arrest_count]
             sources.append(source)
-            
         }
-        tableView.reloadData()
+        DispatchQueue.main.async {
+            [unowned self] in
+            self.tableView.reloadData()
+        }
     }
     
+   
+    
     func loadError() {
-        let alert = UIAlertController(title: "Loading Error",
-                                      message: "There was a problem loading the news feed",
-                                      preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
-        present(alert, animated: true, completion: nil) }
+        DispatchQueue.main.async {
+            [unowned self] in
+            let alert = UIAlertController(title: "Loading Error",
+                                          message: "There was a problem loading the Theft Data...someone may have stole it",
+                                          preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sources.count
+    }
     
-    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = sources[indexPath.row]
+        cell.textLabel?.text = source["Team"]
+        cell.detailTextLabel?.text = source["Team_name"]
+        return cell
+    }
 }
 
 
